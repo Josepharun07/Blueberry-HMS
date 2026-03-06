7d5dd28bdc48a8d95b313d6532ba8cea5936b18a
Josepharun07 <delvinjoseph07@gmail.com>
2026-03-06 17:29:44 +1100

    phase 4 completed and tested

--- setup.sh
+++
@@ -1,7 +0,0 @@
-cd /mnt/c/Users/Arun\ Joseph/Documents/blueberryhills/blueberry-hms/apps/api-core
-
-# Create auth directories
-mkdir -p src/modules/auth/dto
-mkdir -p src/modules/auth/strategies
-mkdir -p src/common/guards
-mkdir -p src/common/decorators
